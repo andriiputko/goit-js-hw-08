41ce204e18e32a6f6c96f67a8c9d8e726c4f7bbe
Andrii <andriiputko@gmail.com>
2023-06-05 21:12:46 +0200

    Hw done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl)
+galleryItems.forEach(element => {
+    galleryEl.insertAdjacentHTML("beforeend",`<li class="gallery__item">
+    <a class="gallery__link" href="${element.original}">
+       <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+    </a>
+ </li>`)
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
